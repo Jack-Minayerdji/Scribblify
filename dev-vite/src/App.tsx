6162588a9b1c56ec295895e52c0d940a0dde03ef
Jack-Minayerdji <jack.minayerdji@gmail.com>
2025-09-21 18:17:15 +0200

    Ininitlized Setup

---
+++ dev-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl">Hello</h1>
+    </div>
+  )
+}
+
+export default App
