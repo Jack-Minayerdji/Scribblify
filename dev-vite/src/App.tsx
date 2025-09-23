b0a992fe05d0d00fd0da812490ee9aa9aecf3c43
Jack-Minayerdji <jack.minayerdji@gmail.com>
2025-09-23 08:15:21 +0200

    default inputs

--- dev-vite/src/App.tsx
+++ dev-vite/src/App.tsx
@@ -1,10 +1,18 @@
 import './index.css'
 
 function App() {
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl">Hello</h1>
+    <div className="flex justify-center items-center h-screen bg-gray-500">
+      <div className="flex">
+        <span>
+          <h1>Width</h1>
+          <input className="" type="number" placeholder="800"/>
+        </span>
+        <span>
+          <h1>Height</h1>
+          <input className="" type="number" placeholder="400"/>
+        </span>
+      </div>
     </div>
   )
 }
